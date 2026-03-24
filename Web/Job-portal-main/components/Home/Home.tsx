80bcd9e506fa7985dc8383490944977152e65775
Banu <klaudius@andalsoftware.com>
2026-03-25 00:29:10 +0700

    Fix: Job category section part deleted

--- Web/Job-portal-main/components/Home/Home.tsx
+++ Web/Job-portal-main/components/Home/Home.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import { Hero, JobCategory } from '../../paths'
+import { Hero } from '../../paths'
 import FeatureJobs from './FeatureJobs'
 
 const Home = () => {
     return (
         <div>
             <Hero />
-            <JobCategory />
             <FeatureJobs />
         </div>
     )
