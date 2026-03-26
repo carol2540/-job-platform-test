5ddd7d401adf6c6d582fda6a48971d465af7b9c1
Banu <klaudius@andalsoftware.com>
2026-03-26 14:40:19 +0700

    Fix: Deleted prisma, use Supabase

--- Web/Job-portal-main/next.config.mjs
+++ Web/Job-portal-main/next.config.mjs
@@ -3,9 +3,6 @@ const nextConfig = {
     images: {
       domains: ['lh3.googleusercontent.com', 'images.unsplash.com', 'via.placeholder.com', 'img.clerk.com'],
     },
-    experimental: {
-      serverComponentsExternalPackages: ['@prisma/client', 'prisma'],
-    },
   }
 
   export default nextConfig
